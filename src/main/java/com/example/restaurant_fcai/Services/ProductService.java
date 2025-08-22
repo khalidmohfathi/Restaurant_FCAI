945880ebed6740cbaea345e4537f2182330dbb18
Zyrex24 <ahmedniazi24a.b@gmail.com>
2025-08-22 05:42:51 +0300

    feature: implement product management API

--- src/main/java/com/example/restaurant_fcai/Services/ProductService.java
+++ src/main/java/com/example/restaurant_fcai/Services/ProductService.java
@@ -5,13 +5,29 @@ import com.example.restaurant_fcai.Repos.ProductRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class ProductService {
 
     @Autowired
     private ProductRepo productRepo;
 
-    public Product saveProduct (Product product) {
-      return  productRepo.save(product);
+    public Product saveProduct(Product product) {
+        return productRepo.save(product);
+    }
+
+    public List<Product> getAllProducts() {
+        return productRepo.findAll();
+    }
+
+    public Product getProductById(Long id) {
+        Optional<Product> product = productRepo.findById(id);
+        return product.orElse(null);
+    }
+
+    public void deleteProduct(Long id) {
+        productRepo.deleteById(id);
     }
 }
